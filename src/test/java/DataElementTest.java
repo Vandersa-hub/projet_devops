4026883e5d103edfe0fd80f0c359cab086ee218b
RadFu <Guillaume.Anthoine@etu.univ-grenoble-alpes.fr>
2022-04-14 17:45:52 +0200

    Test pour la creation d'un DataElement de string

--- src/test/java/DataElementTest.java
+++ src/test/java/DataElementTest.java
@@ -14,12 +14,27 @@ public class DataElementTest {
         assertEquals(dataElementTest.getElements(), arrayTest);
     }
 
+    @Test
+    public void shouldCreateDataElementString(){
+        ArrayList<String> arrayTest = generateStringArrayTest();
+        DataElement<String> dataElementTest = new DataElement<>("StringTest",arrayTest);
+        assertNotNull("DataElement not null String",dataElementTest);
+        assertEquals(dataElementTest.getElements(),arrayTest);
+    }
+
 
     private ArrayList<Integer> generateIntegerArrayTest() {
         ArrayList<Integer> arrayTest = new ArrayList<>();
         arrayTest.add(1);
         arrayTest.add(199);
         arrayTest.add(23);
+        return arrayTest;
+    }
+
+    private ArrayList<String> generateStringArrayTest() {
+        ArrayList<String> arrayTest= new ArrayList<>();
+        arrayTest.add("a");
+        arrayTest.add("Lorem Ipsum !");
         return arrayTest;
     }
 }
