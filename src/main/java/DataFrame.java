6f422acbb430d99a9a89f6a41cc73f36618068f0
RadFu <Guillaume.Anthoine@etu.univ-grenoble-alpes.fr>
2022-04-14 17:53:06 +0200

    Utilisation de DataElement dans DataFrame

--- src/main/java/DataFrame.java
+++ src/main/java/DataFrame.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class DataFrame<E> {
-    private Map<String, List<E>> dataArray;
+public class DataFrame<DataElement> {
+    private Map<String, DataElement> dataArray;
 
     public DataFrame() {
-        this.dataArray = new HashMap<String, List<E>>();
+        this.dataArray = new HashMap<>();
+    }
+
+    public DataFrame(ArrayList<DataElement> data, ArrayList<String> columnsNames) {
+        for (String columnsName : columnsNames) {
+            this.dataArray.put(columnsName, data.get(columnsNames.indexOf(columnsName)));
+        }
     }
 }
