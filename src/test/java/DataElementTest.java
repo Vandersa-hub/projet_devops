23b3208cbbc51c5a65e24c9bd0988815922d889b
RadFu <Guillaume.Anthoine@etu.univ-grenoble-alpes.fr>
2022-04-14 17:45:26 +0200

    Test pour la creation d'un DataElement d'integer

--- src/test/java/DataElementTest.java
+++ src/test/java/DataElementTest.java
@@ -1,2 +1,25 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class DataElementTest {
+    @Test
+    public void shouldCreateDataElementInteger() {
+        ArrayList<Integer> arrayTest = generateIntegerArrayTest();
+        DataElement<Integer> dataElementTest = new DataElement<>("IntegerTest", arrayTest);
+        assertNotNull("DataElement not null Integer", dataElementTest);
+        assertEquals(dataElementTest.getElements(), arrayTest);
+    }
+
+
+    private ArrayList<Integer> generateIntegerArrayTest() {
+        ArrayList<Integer> arrayTest = new ArrayList<>();
+        arrayTest.add(1);
+        arrayTest.add(199);
+        arrayTest.add(23);
+        return arrayTest;
+    }
 }
