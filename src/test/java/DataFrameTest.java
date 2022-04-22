4201b487f4f8356439e47f4f23e9c447b1e57448
Alexis Vandersteene <alexis.vandersteene@gmail.com>
2022-04-22 13:47:36 +0200

    Refactor : Complétion des tests pour l'affichage d'un nombre de ligne

--- src/test/java/DataFrameTest.java
+++ src/test/java/DataFrameTest.java
@@ -24,11 +24,22 @@ public class DataFrameTest {
 
     @Test
     public void shouldDisplayNFirstLine() {
-        String targetDisplay = "Ligne matiere nbeleves moyenne\n" +
-                "0 anglais 41 10.2\n" +
-                "1 francais 23 14.2";
+        String targetDisplay = "Ligne matiere nbeleves moyenne\n";
+        assertEquals(targetDisplay, dataFrame.displayFirstLines(0));
 
+        targetDisplay += "0 anglais 41 10.2";
+        assertEquals(targetDisplay, dataFrame.displayFirstLines(1));
+
+        targetDisplay += "\n1 francais 23 14.2";
         assertEquals(targetDisplay, dataFrame.displayFirstLines(2));
+
+        targetDisplay += "\n2 mathematiques 32 7.2";
+        assertEquals(targetDisplay, dataFrame.displayFirstLines(3));
+
+        targetDisplay += "\n3 histoire 12 11.4";
+        assertEquals(targetDisplay, dataFrame.displayFirstLines(4));
+
+        assertEquals(null, dataFrame.displayFirstLines(5));
     }
 
 }
